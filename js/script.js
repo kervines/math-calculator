d12ff2b0371db666cebf7e826b21cdce4f7c25eb
kervines <kervinzero@gmail.com>
2023-05-13 18:27:09 -0300

    adicionado color mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 const result = document.querySelector('.result');
 const buttons = document.querySelectorAll('.btn');
-// const clear = document.querySelector('.cln');
+const normal = document.querySelector('.normal');
+const dark = document.querySelector('.dark');
+const light = document.querySelector('.light');
+const body = document.body;
+
+normal.addEventListener('click', () => {
+  if (
+    body.classList.contains('dark-mode') ||
+    body.classList.contains('light-mode')
+  ) {
+    body.classList.remove('dark-mode');
+    body.classList.remove('light-mode');
+  }
+});
+dark.addEventListener('click', () => {
+  if (body.classList.contains('light-mode')) {
+    body.classList.remove('light-mode');
+  }
+  body.classList.add('dark-mode');
+});
+light.addEventListener('click', () => {
+  if (body.classList.contains('dark-mode')) {
+    body.classList.remove('dark-mode');
+  }
+  body.classList.add('light-mode');
+});
 
 buttons.forEach((e) => {
   e.addEventListener('click', () => {
